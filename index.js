3da100906d8617c36831419ed170d640ef927eb8
LucasRobertoSilva <fluizfabiano02@gmail.com>
2025-07-07 19:51:56 -0300

    V1.4
    Melhora no UI e adição de responsividade no index

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-function ToggleVisibility(){
-    const element = document.getElementById('aside')
+const searchItems = document.getElementById('search-bar')
+const aside = document.getElementById('aside')
+const card = document.querySelectorAll('.card')
+const searchButton = document.getElementById('search-button')
 
-    element.classList.toggle('hidden')
+// search visibility
+
+const menu = document.getElementById('menu')
+const loginItems = document.querySelector('.perfil-items')
+
+document.addEventListener('DOMContentLoaded', function() {
+  if(window.innerWidth <= 700){
+    searchItems.classList.add('hidden')
+    searchButton.classList.remove('hidden')
+  }
+}); 
+
+function ToggleVisibility(){
+
+  aside.classList.toggle('hidden')
+
+  card.forEach(x => x.classList.toggle('minicard'));
+}
+
+function ToggleSearchVisibility(){
+
+  menu.classList.toggle('hidden')
+  loginItems.classList.toggle('hidden')
+  searchItems.classList.toggle('hidden')
+  document.getElementById('search-button').classList.toggle('hidden')
+
+  document.getElementById('search-input').focus()
+
+  document.getElementById('back').classList.toggle('hidden')
+
 }
 
 function ToggleDarkMode() {
